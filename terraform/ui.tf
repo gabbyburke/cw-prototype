c41a91647282e8419e85a0596904803d1b5acf66
Thomaz S <thomazsilva+prototype@google.com>
2025-09-02 16:16:08 -0400

    Revert "Add explicit `depends_on` to `google_cloudbuild_trigger` resources to depend on their respective `google_storage_bucket_object` resources"
    This reverts commit 00bf5a10f35f68c094aa8d6c88f81a73bb69448e.

--- terraform/ui.tf
+++ terraform/ui.tf
@@ -116,8 +116,7 @@ resource "google_cloudbuild_trigger" "ui" {
     google_storage_bucket_iam_member.cloud_build_deployer,
     google_project_iam_member.cloud_build_deployer,
     google_artifact_registry_repository_iam_member.cloud_build_deployer,
-    time_sleep.wait_for_iam_propagation,
-    google_storage_bucket_object.ui_tarball
+    time_sleep.wait_for_iam_propagation
   ]
 }
 
