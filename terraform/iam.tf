1daf8beb1489968a2884ae074cdc3787bcaf8197
Thomaz S <thomazsilva+prototype@google.com>
2025-09-03 03:20:04 -0400

    Attempt to fix the IAM policy issues

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -14,7 +14,7 @@
 
 # Cloud Build Builder
 resource "google_service_account" "cloud_build_deployer" {
-  account_id   = regex("^(.*?)(?:-)?$", substr("${local.prefix}cloud-build", 0, 30))[0]
+  account_id   = var.service_account_name
   display_name = "Cloud Build builder"
   description  = "Service account used by Cloud Build triggers to deploy Cloud Run services"
 }
